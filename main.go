485e4ad6f62226cb9226960690e49be1948a823f
unknown <kartikgarg938@gmail.com>
2022-05-02 19:32:25 +0530

    very small email verifier

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+/*
+@bufio - to parse from terminal to the program
+*/
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
+
+	//for sends arguments one by one to the method below
+	for scanner.Scan(){
+		checkDomain(scanner.Text())
+	}
+
+	if err:= scanner.Err(); err!=nil{
+		log.Fatal("Could not read from input: %v\n", err)
+	}
+}
+
+func checkDomain(domain string){
+	var hasMX, hasDMARC, hasSPF bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err!=nil{
+		log.Printf("error: %v\n", err)
+	}
+
+	if len(mxRecords)>0 {
+		hasMX = true
+	}
+
+	txtRecord, err := net.LookupTXT(domain)
+	if err != nil{
+		log.Printf("Error: %v\n", err)
+	}
+	/*
+	_ => is the iterator and the record here is the every single value present in the txtRecord
+	*/
+	for _, record := range txtRecord{
+		if strings.HasPrefix(record, "v=spf1"){
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+
+	if err!=nil{
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords{
+		if strings.HasPrefix(record, "v=DMARC1"){
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+}
